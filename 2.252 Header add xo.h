017d392c60831d9533e029765e3a3c362a0c905b
sharifahmeeed <62994850+sharifahmeeed@users.noreply.github.com>
2021-07-06 15:14:31 +0600

    Create 2.252 Header add xo.h

---
+++ 2.252 Header add xo.h
@@ -0,0 +1,9 @@
+#ifndef XO_H_INCLUDED
+#define XO_H_INCLUDED
+
+void displayGridDefault();
+void displayGrid(int game[][3]); // prototype if we put our function below the main
+void play(int game[][3]);// prototype
+int checkWin(int game[][3], int player);// prototype
+
+#endif // XO_H_INCLUDED
